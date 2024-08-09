e5d6465939715c2d4ba09076f22c148af6d751bc
Rodrigo Roberto de Almeida <rodrigo.roberto.almeida@gmail.com>
2024-08-09 00:25:11 -0300

    updated ghost effect

--- data/scripts/talkactions/gm/ghost.lua
+++ data/scripts/talkactions/gm/ghost.lua
@@ -1,22 +1,22 @@
 local ghost = TalkAction("/ghost")
 
 function ghost.onSay(player, words, param)
-	-- create log
-	logCommand(player, words, param)
+    -- create log
+    logCommand(player, words, param)
 
-	local position = player:getPosition()
-	local isGhost = not player:isInGhostMode()
+    local position = player:getPosition()
+    local isGhost = not player:isInGhostMode()
 
-	player:setGhostMode(isGhost)
-	if isGhost then
-		player:sendTextMessage(MESSAGE_HOTKEY_PRESSED, "You are now invisible.")
-		position:sendMagicEffect(CONST_ME_YALAHARIGHOST)
-	else
-		player:sendTextMessage(MESSAGE_HOTKEY_PRESSED, "You are visible again.")
-		position.x = position.x + 1
-		position:sendMagicEffect(CONST_ME_SMOKE)
-	end
-	return true
+    player:setGhostMode(isGhost)
+    if isGhost then
+        player:sendTextMessage(MESSAGE_HOTKEY_PRESSED, "You are now invisible.")
+        position:sendMagicEffect(CONST_ME_YALAHARIGHOST)
+    else
+        player:sendTextMessage(MESSAGE_HOTKEY_PRESSED, "You are visible again.")
+        position.x = position.x + 1
+        position:sendMagicEffect(CONST_ME_AVATAR_APPEAR)
+    end
+    return true
 end
 
 ghost:separator(" ")

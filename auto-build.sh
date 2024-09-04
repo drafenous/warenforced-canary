cbcc40b4c69caae305e79d55a476c144ed837f44
Rodrigo Roberto de Almeida <rodrigo.roberto.almeida@gmail.com>
2024-09-04 02:41:31 -0300

    created auto-build script

--- auto-build.sh
+++ auto-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 BIN_PATH=${1:-"./canary"}
-
+rm -rf ./build
+echo "removing build folder"
+echo "running build"
 mkdir build && cd build
 
 cmake -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake .. --preset linux-release

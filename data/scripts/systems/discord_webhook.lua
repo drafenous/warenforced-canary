6264520ecffa4bd53191e909b318c1d7d96114f0
Rodrigo Roberto de Almeida <rodrigo.roberto.almeida@gmail.com>
2024-09-25 21:54:34 -0300

    updated discord webhooks

--- data/scripts/systems/discord_webhook.lua
+++ data/scripts/systems/discord_webhook.lua
@@ -3,11 +3,11 @@
 -- Leave empty if you wish to disable.
 if not announcementChannels then
     announcementChannels = {
-        ["serverAnnouncements"] = "", -- Used for an announcement channel on your discord
-        ["raids"] = "", -- Used to isolate raids on your discord
-        ["player-kills"] = "", -- Self-explaining
-        ["player-levels"] = "", -- Self-explaining
-        ["reports"] = ""
+        ["serverAnnouncements"] = "https://discord.com/api/webhooks/1272738605445414962/6rLqRdOLsDN9d2Lo_UGGqEZoq98cUfqJM20khUrceUCHpn4359JudnL0fETX6W65eQyt", -- Used for an announcement channel on your discord
+        ["raids"] = "https://discord.com/api/webhooks/1288663766405353534/lxpOOyUBtvK0AdzyRe2nZXOFjfYYx_8bN9AQsfGo720_aCTh7bwdyxnd8eIxbY37L9vu", -- Used to isolate raids on your discord
+        ["player-kills"] = "https://discord.com/api/webhooks/1288664131305734144/WE8srzTRAdG1JOe6CR_P2Pa1kpTCDaCHdWeW__3Sfz4fdZh_DgujPRN81SZ3yA_Q8yLT", -- Self-explaining
+        ["player-levels"] = "https://discord.com/api/webhooks/1288664373333721088/fo7oyxElOWZYn41KNr4XWJAyONu2s6W2gOezSn1kI5U3eurRMRmyRsXt2abL2HH7y0TA", -- Self-explaining
+        ["reports"] = "https://discord.com/api/webhooks/1288664576241700864/yjmK-a84ZT6nQZ_fu9VYrmnwRwaq2in1e5rPtDnmHaZ5Q_bWGr_y9cBO5KTryk5l3KZj"
     }
 end
 
